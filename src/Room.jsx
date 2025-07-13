400e23debbd497778a082b61a0de6a0e4bb8cc2f
seok-young <nsy7160@gmail.com>
2025-07-13 12:43:09 +0900

    feat:roomUI

--- src/Room.jsx
+++ src/Room.jsx
@@ -1,9 +1,31 @@
 import {observer} from 'mobx-react-lite'
+import { IconButton, Stack } from '@mui/material'
+import VolumeOffIcon from '@mui/icons-material/VolumeOff'
+import VolumeUpIcon from '@mui/icons-material/VolumeUp'
+import ExitToAppIcon from '@mui/icons-material/ExitToApp'
 
 function Room() {
     return (
-        <div>
-            Room page
+        <div style={{position:'relative', width:'100%', height:'100vh'}}>
+            <video autoPlay muted
+                    style={{position:'absoulte', 
+                            left:'0px', top:'0px', width:'100%', height:'100%',
+                            background: 'black', objectFit:'contain'}}/>
+            <video autoPlay muted
+                    style={{position:'absolute',
+                            right:'24px', bottom:'24px', width:'35%', height:'35%',
+                            background: 'transparent', objectFit:'contain'}}/>
+            <Stack direction='row' spacing={1}
+                   sx={{justifyContent:'flex-end', alignItems:'center',
+                        position:'absolute', top:'16px', right:'16px'}}>
+                <IconButton style={{color:'red'}}>
+                    <VolumeUpIcon />
+                </IconButton>
+
+                <IconButton style={{color:'lightgrey'}}>
+                    <ExitToAppIcon />
+                </IconButton>
+            </Stack>
         </div>
     );
 }
